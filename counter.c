cb3a903dd96c203708dad43626093524868324ac
Vivanco Qs <86332346+Vivanco443@users.noreply.github.com>
2021-08-18 17:37:05 -0500

    Contamos milisegundos
    Se hicieron modificaciones para solo contar milisegundos, ya que para nuestros propositos no necesitamos segundos enteros

--- counter.c
+++ counter.c
@@ -9,7 +9,7 @@ int main () {
     struct timeval begin, end;
     gettimeofday(&begin, 0);
     
-    int iterations = 1000*1000*1000;
+    int iterations = 1000*1000*22;
     for (int i=0; i<iterations; i++) {
         sum += add;
         add /= 2.0;
@@ -17,13 +17,14 @@ int main () {
     
     // Stop measuring time and calculate the elapsed time
     gettimeofday(&end, 0);
-    long seconds = end.tv_sec - begin.tv_sec;
+    // long seconds = end.tv_sec - begin.tv_sec;    // Para contar segundos utilizamos
     long microseconds = end.tv_usec - begin.tv_usec;
-    double elapsed = seconds + microseconds*1e-6;
+    double miliseconds = microseconds * 1e-3;
+    //double elapsed = seconds + microseconds*1e-6;     // se suman usegundos y segundos
     
-    printf("Result: %.20f\n", sum);
+    //printf("Result: %.6f\n", sum);
     
-    printf("Time measured: %.3f seconds.\n", elapsed);
+    printf("\nTime measured: %.3f miliseconds.\n\n", miliseconds);
     
     return 0;
 }
