4c63d443c7cb7381cf448243fc4c300e8716a3e6
Vivanco Qs <86332346+Vivanco443@users.noreply.github.com>
2021-08-18 16:57:07 -0500

    Código de internet
    La <chrono>biblioteca tiene acceso a algunos relojes diferentes en su máquina, cada uno de ellos con diferentes propósitos y características. Si lo desea, puede obtener más detalles sobre cada tipo de reloj aquí . Pero, a menos que realmente necesite un reloj diferente, recomendaría simplemente usar high_resolution_clock. Éste usa el reloj con la resolución más alta disponible, por lo que probablemente sea adecuado para la mayoría de las personas. He aquí cómo usarlo:

--- counter.c
+++ counter.c
@@ -1,13 +1,26 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <chrono>
 
-unsigned long tiempo = 0;
+int main () {
+    double sum = 0;
+    double add = 1;
 
-int main(){
-    int i;
+    // Start measuring time
+    auto begin = std::chrono::high_resolution_clock::now();
     
-    while (i < 200){
-        i++;
+    int iterations = 1000*1000*1000;
+    for (int i=0; i<iterations; i++) {
+        sum += add;
+        add /= 2.0;
     }
-    tiempo = millis();
-    printf(tiempo);
+    
+    // Stop measuring time and calculate the elapsed time
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    
+    printf("Result: %.20f\n", sum);
+    
+    printf("Time measured: %.3f seconds.\n", elapsed.count() * 1e-9);
+    
+    return 0;
 }
